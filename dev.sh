03487e7b2028d9be08112222a0a1d8ac8cceaa61
Stanislav Sysoev <d4rkr00t@gmail.com>
2021-12-25 16:37:08 +1100

    build: add test coverage rule

--- dev.sh
+++
@@ -1,2 +0,0 @@
-echo "Devloop for EVO..."
-watchexec -w ./ -e go "go build main.go  && cp ./main ~/.bin-temp/evo"
--- test.sh
+++
@@ -1,7 +0,0 @@
-if [ "$1" == "-v" ]; then
-  go test ./lib -v
-elif [ "$1" == "-c" ]; then
-  go test ./lib -coverprofile cov.html && go tool cover -html=cov.html
-else
-  go test ./lib $@
-fi
